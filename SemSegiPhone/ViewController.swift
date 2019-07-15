b4ff493e4004b549c05ae1305909601dbcefe533
Sachin <sacmehta@D-10-19-112-99.dhcp4.washington.edu>
2019-07-15 14:16:01 -0700

    Using FP16 model

--- SemSegiPhone/ViewController.swift
+++ SemSegiPhone/ViewController.swift
@@ -133,7 +133,7 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     
     
     func setupVisionModel() {
-        guard let visionModel = try? VNCoreMLModel(for: espnetv2_16_bits().model) else{
+        guard let visionModel = try? VNCoreMLModel(for: espnetv2_fp16().model) else{
             fatalError("Can not load CNN model")
         }
         
